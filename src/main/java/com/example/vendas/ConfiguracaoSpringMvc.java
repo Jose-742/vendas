bd502d6be242d33dc9f1864c36e7421a164f9b89
Jose-742 <josenatalrb@gmail.com>
2022-10-20 22:47:41 -0300

    Projeto finalizado

--- src/main/java/com/example/vendas/ConfiguracaoSpringMvc.java
+++ src/main/java/com/example/vendas/ConfiguracaoSpringMvc.java
@@ -8,11 +8,9 @@ public class ConfiguracaoSpringMvc implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
       /*  registry.addViewController("/").setViewName("home");
-        registry.addViewController("/home").setViewName("home");*/
-        
+        registry.addViewController("/home").setViewName("home");*/        
         //registry.addViewController("/").setViewName("produto/list");//produtos
-    	//registry.addViewController("/home").setViewName("produto/list");
-    	
+    	//registry.addViewController("/home").setViewName("produto/list");   	
         registry.addRedirectViewController("/", "produtos/list");
         registry.addRedirectViewController("/home", "produtos/list");    
        
