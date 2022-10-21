e1cd1d86c220b434e269c54d833ee1832752869b
Jose-742 <josenatalrb@gmail.com>
2022-10-20 23:01:49 -0300

    Projeto finalizado

--- src/main/java/com/example/vendas/VendasApplication.java
+++ src/main/java/com/example/vendas/VendasApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VendasApplication {
 	//private static final Logger LOG = LoggerFactory.getLogger(VendasApplication.class);
-	
-	
+		
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
 		//LOG.info("A aplicação de gerenciamento de sessão iniciou corretamente");
